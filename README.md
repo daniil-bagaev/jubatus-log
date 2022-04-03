cd1ba334ee2fbbcd134083a29c838ede30260e37
Daniil Bagaev <daniil.bagaev@yahoo.com>
2022-04-03 22:35:40 +0500

    Add new options and functions them processing, add more documentary to README

--- README.md
+++ README.md
@@ -1,38 +1,126 @@
 ## Install
 ```
-$ npm install jubatus-log
+$ npm install jub-log
+```
+## Usage
+```js
+    log = require('jub-log');
+    log('Hello World', {
+        type: 'info'
+    });
 ```
 ## API
-### `opts.level` 
-bla bla `Default: Your terminal supports color level`
-### `opts.logdir` 
+#### `opts.type`
+bla bla `Default: custom`
+- `info`
+- `help`
+- `error`
+- `warning`
+#### `opts.level` 
+`Default: Your terminal supports color level` 
+Specifies the level of color support.
+Color support is automatically detected, but you can override it by setting the `level` property.
+You can set any level from 0 to 3, but no more than what your terminal allows.
+| Level    	| Description                           |
+|-------	|---------------------------------------|
+| 0         | All colors disabled                   |
+| 1         | Basic color support (16 colors)       |
+| 2         | 256 color support                     |
+| 3         | Truecolor support (16 million colors) |
+Can be overriden by the user with the flags `--color` and `--no-color`.
+#### `opts.logdir` 
 bla bla `Default: ./.log/` 
-### `opts.filename` 
+#### `opts.filename` 
 bla bla `Default: null`
-### `opts.header`
+#### `opts.header`
 bla bla `Default: null`
-### `opts.prefix`
+#### `opts.prefix`
+`Default: null`
+Add text before your message
+#### `opts.suffix`
+`Default: null`
+Add text after your message.
+### Styles
+Each style has an `open` and `close` property.
+#### `opts.color`
+The text color
+`Default: 'white'` 
+Standart options:
+- `black`
+- `red`
+- `green`
+- `yellow`
+- `blue`
+- `magenta`
+- `cyan`
+- `white`
+- `grey` alias(`gray`, `brightBlack`)
+- `brightRed`
+- `brightGreen`
+- `brightYellow`
+- `brightBlue`
+- `brightMagenta`
+- `brightCyan`
+- `brightWhite`
+also you can use hex or rgb variant if your terminal support them.
+#### `opts.bg`
+The text background color
+`Default: 'black'` 
+Standart options:
+- `black`
+- `red`
+- `green`
+- `yellow`
+- `blue`
+- `magenta`
+- `cyan`
+- `white`
+- `grey` alias(`gray`, `brightBlack`)
+- `brightRed`
+- `brightGreen`
+- `brightYellow`
+- `brightBlue`
+- `brightMagenta`
+- `brightCyan`
+- `brightWhite`
+also you can use hex or rgb variant if your terminal support them.
+#### `opts.style`
+`Default: 'standart'`
+Standart options:
+- `standart` (alias `reset`) - reset the current style.
+- `bold` - make the text bold.
+- `dim` - make the text have lower opacity.
+- `italic` - make the text italic.
+- `underline` - put horizontal line below the text.
+- `overline` - put horizontal line above the text.
+- `inverse` - invert background and foreground colors.
+- `hidden` - make the text invisible.
+- `strikethrough` - put horizontal line through the center of the text.
+#### `opts.date`
 bla bla `Default: null`
-### `opts.suffix`
-bla bla `Default: null`
-### `opts.color`
-bla bla `Default: 'white'`
-### `opts.bg`
-bla bla `Default: 'black'`
-### `opts.style`
-bla bla `Default: 'standart'`
-- `reset`, `standart`
-- `bold`
-- `dim`
-- `italic`
-- `underline`
-- `overline`
-- `inverse`
-- `hidden`
-- `strikethrough`
-### `opts.date`
-bla bla `Default: null`
+|             	| Token 	| Output                                  	|
+|-------------	|-------	|-----------------------------------------	|
+| Years       	| YY    	| 70, 71 ... 21, 22                       	|
+|             	| YYYY  	| 1970, 1971 ... 2021, 2022               	|
+| Months      	| M     	| 1, 2 ... 11, 12                         	|
+|             	| MM    	| 01, 02 ... 11, 12                       	|
+|             	| MMM   	| Jan, Feb ... Nov, Dec                   	|
+|             	| MMMM  	| January, Febrary ... November, December 	|
+| Days        	| D     	| 1, 2 ... 30, 31                         	|
+|             	| DD    	| 01, 02 ... 30, 31                       	|
+| Hours       	| H     	| 1, 2 ... 23, 00                         	|
+|             	| HH    	| 01, 02, ... 23, 00                      	|
+| Minutes     	| m     	| 1, 2 ... 58, 59                         	|
+|             	| mm    	| 01, 02 ... 58, 59                       	|
+| Seconds     	| s     	| 1, 2 ... 58, 59                         	|
+|             	| ss    	| 01, 02 ... 58, 59                       	|
+| Miliseconds 	| S     	| 1, 2 ... 8, 9                           	|
+|             	| SS    	| 01, 02 ... 98, 99                       	|
+|             	| SS    	| 001, 002, ... 998, 999                  	|
+## Example
+
 ## Related
-- [supports-color](https://github.com/chalk/supports-color) code for supcolor
+- [supports-color](https://github.com/chalk/supports-color) code for colors suport 
+- [ansi-styles](https://github.com/chalk/ansi-styles) code for ansi-codes support
 ## License
 MIT
--- index.js
+++ index.js
@@ -100,82 +100,382 @@ const
             style: 'standart',
             date: null
         },
+        types: {
+            info: {
+                color: 'blue',
+                header: '[Info]'
+            },
+            help: {
+                color: 'cyan',
+                header: '[Help]'
+            },
+            error: {
+                color: 'brightRed',
+                header: '[Error]'
+            }, 
+            warning: {
+                color: 'yellow',
+                header: '[Warning]'
+            }
+        },
         styleCodes(style) {
             switch(style) {
                 case 'standart':
                 case 'reset':
                     return { 
                         open: '\u001b[0m', 
-                        close: '\u001b[0m', 
+                        close: '\u001b[0m'
                     };
                 case 'bold':
                     return { 
                         open: '\u001b[1m', 
-                        close: '\u001b[22m', 
+                        close: '\u001b[22m'
                     };
                 case 'dim':
                     return { 
                         open: '\u001b[2m', 
-                        close: '\u001b[22m', 
+                        close: '\u001b[22m'
                     };
                 case 'italic':
                     return { 
                         open: '\u001b[3m', 
-                        close: '\u001b[23m', 
+                        close: '\u001b[23m'
                     };
                 case 'underline':                    
                     return { 
                         open: '\u001b[4m', 
-                        close: '\u001b[24m', 
+                        close: '\u001b[24m'
                     };
                 case 'overline':
                     return { 
                         open: '\u001b[53m', 
-                        close: '\u001b[55m', 
+                        close: '\u001b[55m'
                     };
                 case 'inverse':                    
                     return { 
                         open: '\u001b[7m', 
-                        close: '\u001b[27m', 
+                        close: '\u001b[27m'
                     };
                 case 'hidden':                    
                     return { 
                         open: '\u001b[8m', 
-                        close: '\u001b[28m', 
+                        close: '\u001b[28m'
                     };
                 case 'strikethrough':
                     return { 
                         open: '\u001b[9m', 
-                        close: '\u001b[29m', 
+                        close: '\u001b[29m'
                     };
                 default:                    
                     return { 
                         open: '\u001b[0m', 
-                        close: '\u001b[0m', 
+                        close: '\u001b[0m'
                     };
             }
         },
-        colorCodes(color, level) {
-            if(!level)
-                level = this.env.level;
+        bgNameCodes(color) {
+            let closeCode = '\u001b[49m';
+            switch(color) {
+                case 'white':
+                    return {
+                        open: '\u001b[47m',
+                        close: closeCode
+                    };
+                case 'black':
+                    return {
+                        open: '\u001b[40m',
+                        close: closeCode
+                    };
+                case 'red':
+                    return {
+                        open: '\u001b[41m',
+                        close: closeCode
+                    };
+                case 'green':
+                    return {
+                        open: '\u001b[42m',
+                        close: closeCode
+                    };
+                case 'yellow':
+                    return {
+                        open: '\u001b[43m',
+                        close: closeCode
+                    };
+                case 'blue':
+                    return {
+                        open: '\u001b[44m',
+                        close: closeCode
+                    };
+                case 'magenta':
+                    return {
+                        open: '\u001b[45m',
+                        close: closeCode
+                    };
+                case 'cyan':
+                    return {
+                        open: '\u001b[46m',
+                        close: closeCode
+                    };
+                case 'gray':
+                case 'grey':
+                case 'brightBlack': 
+                    return {
+                        open: '\u001b[100m',
+                        close: closeCode
+                    };
+                case 'brightWhite':
+                    return {
+                        open: '\u001b[107m',
+                        close: closeCode
+                    };
+                case 'brightRed':
+                    return {
+                        open: '\u001b101m',
+                        close: closeCode
+                    };
+                case 'brightGreen':
+                    return {
+                        open: '\u001b[102m',
+                        close: closeCode
+                    };
+                case 'brightYellow':
+                    return {
+                        open: '\u001b[103m',
+                        close: closeCode
+                    };
+                case 'brightBlue':
+                    return {
+                        open: '\u001b[104m',
+                        close: closeCode
+                    };
+                case 'brightMagenta':
+                    return {
+                        open: '\u001b[105m',
+                        close: closeCode
+                    };
+                case 'brightCyan':
+                    return {
+                        open: '\u001b[106m',
+                        close: closeCode
+                    };
+                default: 
+                    return {
+                        open: '\u001b[47m',
+                        close: closeCode
+                    };
+            }
+        },
+        colorNameCodes(color) {
+            let closeCode = '\u001b[39m';
+            switch(color) {
+                case 'white':
+                    return {
+                        open: '\u001b[37m',
+                        close: closeCode
+                    };
+                case 'black':
+                    return {
+                        open: '\u001b[30m',
+                        close: closeCode
+                    };
+                case 'red':
+                    return {
+                        open: '\u001b[31m',
+                        close: closeCode
+                    };
+                case 'green':
+                    return {
+                        open: '\u001b[32m',
+                        close: closeCode
+                    };
+                case 'yellow':
+                    return {
+                        open: '\u001b[33m',
+                        close: closeCode
+                    };
+                case 'blue':
+                    return {
+                        open: '\u001b[34m',
+                        close: closeCode
+                    };
+                case 'magenta':
+                    return {
+                        open: '\u001b[35m',
+                        close: closeCode
+                    };
+                case 'cyan':
+                    return {
+                        open: '\u001b[36m',
+                        close: closeCode
+                    };
+                case 'gray':
+                case 'grey':
+                case 'brightBlack': 
+                    return {
+                        open: '\u001b[90m',
+                        close: closeCode
+                    };
+                case 'brightWhite':
+                    return {
+                        open: '\u001b[97m',
+                        close: closeCode
+                    };
+                case 'brightRed':
+                    return {
+                        open: '\u001b91m',
+                        close: closeCode
+                    };
+                case 'brightGreen':
+                    return {
+                        open: '\u001b[92m',
+                        close: closeCode
+                    };
+                case 'brightYellow':
+                    return {
+                        open: '\u001b[93m',
+                        close: closeCode
+                    };
+                case 'brightBlue':
+                    return {
+                        open: '\u001b[94m',
+                        close: closeCode
+                    };
+                case 'brightMagenta':
+                    return {
+                        open: '\u001b[95m',
+                        close: closeCode
+                    };
+                case 'brightCyan':
+                    return {
+                        open: '\u001b[96m',
+                        close: closeCode
+                    };
+                default: 
+                    return {
+                        open: '\u001b[37m',
+                        close: closeCode
+                    };
+            }
+        },
+        dateFormat(format) {
+            let
+                months = ['M', 'MM', 'MMM', 'MMMM'],
+                days = ['D', 'DD'],
+                years = ['YY', 'YYYY'],
+                hours = ['H', 'HH'],
+                minutes = ['m', 'mm'],
+                seconds = ['s', 'ss'],
+                miliseconds = ['S', 'SS', 'SSS'];
+            let 
+                monthsNames = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+            let date = format
+                .match(/([A-Za-z]{1,4})|([\s\:\-]{0,})/gi)
+                .filter(Boolean)
+                .map(el => {
+                    let z;
+                    if(/[A-Za-z{1,4}]/gi.test(el)){
+                        if(years.includes(el)) {
+                            switch(el) {
+                                case 'YYYY':
+                                    return new Date().getFullYear()+'';
+                                case 'YY':
+                                    return (new Date().getFullYear()+'').slice(2);
+                            }
+                        }
+                        if(months.includes(el)) {
+                            switch(el) {
+                                case 'MMMM':
+                                    return monthsNames[new Date().getMonth()];
+                                case 'MMM':
+                                    return monthsNames[new Date().getMonth()].slice(0, 3);
+                                case 'MM':
+                                    return ((new Date().getUTCMonth()+'').length === 1) ? '0'+new Date().getUTCMonth() : new Date().getUTCMonth()+'';
+                                case 'M':
+                                    return new Date().getUTCMonth()+'';
+                            }    
+                        }
+                        if(days.includes(el)) {
+                            switch(el) {
+                                case 'DD':
+                                    return ((new Date().getDate()+'').length === 1) ? '0'+new Date().getDate() : new Date().getDate()+'';
+                                case 'D':
+                                    return new Date().getDate()+'';
+                            }                            
+                        }
+                        if(hours.includes(el)) {
+                            switch(el) {
+                                case 'HH':
+                                    return ((new Date().getHours()+'').length === 1) ? '0'+new Date().getHours() : new Date().getHours()+'';
+                                case 'H':
+                                    return new Date().getHours()+'';
+                            }                            
+                        }
+                        if(minutes.includes(el)) {
+                            switch(el) {
+                                case 'mm':
+                                    return ((new Date().getMinutes()+'').length === 1) ? '0'+new Date().getMinutes() : new Date().getMinutes()+'';
+                                case 'm':
+                                    return new Date().getMinutes()+'';
+                            }                            
+                        }                    
+                        if(seconds.includes(el)) {
+                            switch(el) {
+                                case 'ss':
+                                    return ((new Date().getSeconds()+'').length === 1) ? '0'+new Date().getSeconds() : new Date().getSeconds()+'';
+                                case 's':
+                                    return new Date().getSeconds()+'';
+                            }                            
+                        }
+                        if(miliseconds.includes(el)) {
+                            switch(el) {
+                                case 'S':
+                                    return (((new Date().getMilliseconds()+'').length === 1) ? '00'+ (new Date().getMilliseconds()+'') : ((new Date().getMilliseconds()+'').length === 2) ? '0'+ (new Date().getMilliseconds()+'') : (new Date().getMilliseconds()+'')).slice(0,1);
+                                case 'SS':
+                                    return (((new Date().getMilliseconds()+'').length === 1) ? '00'+ (new Date().getMilliseconds()+'') : ((new Date().getMilliseconds()+'').length === 2) ? '0'+ (new Date().getMilliseconds()+'') : (new Date().getMilliseconds()+'')).slice(0,2);
+                                case 'SSS':
+                                    return (((new Date().getMilliseconds()+'').length === 1) ? '00'+ (new Date().getMilliseconds()+'') : ((new Date().getMilliseconds()+'').length === 2) ? '0'+ (new Date().getMilliseconds()+'') : (new Date().getMilliseconds()+''));
+                            }                            
+                        }
+                    } else 
+                        return el;
+                })
+                .join('^^')
+                .replaceAll('^^','');
+            return '[' + date + ']';
         },
         log (msg, opts) {
             if(!opts)
-                opts = {};            
-            opts = {
-                filename: (opts.filename || this.opts.filename),
-                logdir: (opts.logdir || this.opts.logdir),
-                header: (opts.header || this.opts.header),
-                color: (opts.color || this.opts.color),
-                bg: (opts.bg || this.opts.bg),
-                style: (opts.style || this.opts.style),
-                prefix: (opts.prefix || this.opts.prefix),
-                suffix: (opts.suffix || this.opts.suffix),
-                level: (this.env.level < opts.level) ? this.env.level : (opts.level || this.env.level)
-            }
+                opts = {};    
+            opts = 
+                (!Object.keys(opts).length) ? this.opts : 
+                    (opts.type && this.types.hasOwnProperty(opts.type)) ? Object.assign(this.opts, this.types[opts.type], opts) : {
+                        type: 'custom',
+                        filename: (opts.filename || this.opts.filename),
+                        logdir: (opts.logdir || this.opts.logdir),
+                        header: (opts.header || this.opts.header),
+                        color: (opts.color || this.opts.color),
+                        bg: (opts.bg || this.opts.bg),
+                        style: (opts.style || this.opts.style), //TODO: return style standart if not in styles
+                        prefix: (opts.prefix || this.opts.prefix),
+                        suffix: (opts.suffix || this.opts.suffix),
+                        date: (opts.date || this.opts.date)
+                    };
+            Object.assign(opts, {level: (this.env.level < opts.level) ? this.env.level : (opts.level || this.env.level)});
+            if(opts.style)
+                opts.styleCodes = this.styleCodes(opts.style);
+
+            if(opts.bg)       
+                if(/[A-Za-z]/i.test(opts.color) && opts.level >=1)
+                    opts.bgCodes = this.bgNameCodes(opts.bg);
+
+            if(opts.color)
+                if(/[A-Za-z]/g.test(opts.color) && opts.level >=1)
+                    opts.colorCodes = this.colorNameCodes(opts.color);
+
             msg = (opts.header && opts.header.length) ? opts.header + '\n' + msg : msg;
             msg = (opts.prefix && opts.prefix.length) ? opts.prefix + msg : msg;
             msg = (opts.suffix && opts.suffix.length) ? msg + opts.suffix : msg;
+            msg = (opts.date && opts.date.length) ? this.dateFormat(opts.date) +' '+ msg : msg;
             if(opts.filename) {
                 fs.mkdirSync(opts.logdir, {recursive: true}, err => {
                     if (err)
@@ -183,10 +483,12 @@ const
                 })
                 fs.appendFileSync(opts.logdir + opts.filename, msg+'\n');
             }
+            //console.log(opts);
+            console.log(msg);
         }
     },
     log = (msg, opts) => {
         logger.log(msg, opts);
     };
-//log('aaa', {style: 'bold'});
-module.exports = log;
+log('Hello world', {type: 'info'});
+//module.exports = log;
